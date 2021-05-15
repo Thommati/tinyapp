3cc268b0b4e34365e79e455ea895091c54ac32dc
Matthew Thompson <supra__charged@hotmail.com>
2021-05-14 19:22:45 -0600

    Add csurf package and implement csrfProtection on forms

--- express_server.js
+++ express_server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cookieSession = require('cookie-session');
+const csrf = require('csurf');
 
 const authRoutes = require('./routes/auth-routes');
 const urlRoutes = require('./routes/url-routes');
 const { users } = require('./data');
 
 const app = express();
+const csrfProtection = csrf();
 const PORT = 8080;
 
 // Options
@@ -20,12 +22,14 @@ app.use(cookieSession({
   name: 'TinySession',
   secret: 'tinyURLsessionSecret'
 }));
+app.use(csrfProtection);
 
 // Make isLoggedIn and user info available in views
 app.use((req, res, next) => {
   const userId = req.session['user_id'];
   res.locals.isLoggedIn = userId ? true : false;
   res.locals.user = users[userId];
+  res.locals.csrfToken = req.csrfToken();
   next();
 });
 
